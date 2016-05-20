1bcbe5882aa6fcde5b0b10135f20929d6b8a3c12
Adrian Albu <whiteadi@yahoo.com>
2016-05-20 13:19:21 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,77 @@
- 
+exercisesTypes = ['push', 'sit', 'squats'];
+
+function showExercise(exerciseType) {
+  $("#home").hide();
+
+  $("#exercise").show();
+
+  $("#startExercise").show();
+
+  $("#stopExercise").hide();
+
+  $("#score").hide();
+
+  $("#savesScore").hide();
+
+  this.currentExercise = exerciseType;
+
+  document.getElementById("exerciseTitle").innerHTML = exerciseType.charAt(0).toUpperCase() + exerciseType.slice(1);
+
+  show();
+}
+
+function startExercise() {
+  $("#startExercise").hide();
+
+  $("#stopExercise").show();
+
+  $("#score").hide();
+
+  $('#score').val('');
+
+  reset();
+
+  start();
+}
+
+function stopExercise() {
+  $("#startExercise").show();
+
+  $("#stopExercise").hide();
+
+  stop();
+
+  $("#score").show();
+
+  $("#savesScore").show();
+}
+
+function saveScore(newScore, exerciseType){
+
+  var oldScore = parseInt(localStorage.getItem(exerciseType));
+
+  if(parseInt(newScore) > oldScore){
+    localStorage.setItem(exerciseType, newScore);    
+  }
+}
+function goHome(){
+
+  getScores();
+
+  $("#home").show();
+
+  $("#exercise").hide();
+
+}
+
+function getScores() {
+  // Check browser support
+  if (typeof(Storage) !== "undefined") {
+    // Retrieve
+    document.getElementById("push").innerHTML = localStorage.getItem("push");
+    document.getElementById("sit").innerHTML = localStorage.getItem("sit");
+    document.getElementById("squats").innerHTML = localStorage.getItem("squats");
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  }
+}
